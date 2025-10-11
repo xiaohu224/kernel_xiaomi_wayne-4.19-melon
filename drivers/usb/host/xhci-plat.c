e26ec1c833b97464f512ad4750d81093f51bf790
xiaohu224 <xiaohuluobo2240@outlook.com>
2025-10-12 03:04:12 +0800

    Revert "usb: host: xhci-plat: add wakeup entry at sysfs"
    This reverts commit 7b0e6ea6b83620f5ff7a4983ee3e6f1b3f5384f2.

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -301,7 +301,7 @@ static int xhci_plat_probe(struct platform_device *pdev)
 			*priv = *priv_match;
 	}
 
-	device_set_wakeup_capable(&pdev->dev, true);
+	device_wakeup_enable(hcd->self.controller);
 
 	xhci->clk = clk;
 	xhci->reg_clk = reg_clk;
